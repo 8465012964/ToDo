ef37958eb1fd86bd98778b989677707701403a2c
vishnupriya <alurivishnupriya>
2023-11-28 09:33:52 +0530

    added

---
+++ src/main/java/controller/SignUp.java
@@ -0,0 +1,72 @@
+package controller;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.Period;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dto.UserDto;
+import service.UserService;
+@WebServlet("/signup")
+public class SignUp extends HttpServlet{
+   @Override
+protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	//int id=Integer.parseInt(req.getParameter("id"));
+	String name=req.getParameter("name");
+	String email=req.getParameter("email");
+	String password=req.getParameter("password");
+	long mobile=Long.parseLong(req.getParameter("mobile"));
+	LocalDate dob=LocalDate.parse(req.getParameter("dob"));
+	String gender=req.getParameter("gender");
+	//int age=LocalDate.now().getYear()-dob.getYear();
+	int age=Period.between(dob, LocalDate.now()).getYears();
+	
+	
+	UserDto dto=new UserDto();
+	dto.setPassword(password);
+	dto.setName(name);
+	// dto.setId(id);
+	dto.setAge(age);
+	dto.setDob(dob);
+	dto.setEmail(email);
+	dto.setGender(gender);
+	dto.setMobile(mobile);
+	
+	UserService service=new UserService();
+	if(service.saveUser(dto)) {
+		resp.getWriter().print("<h1 align='center' style='color:green'>Account Created Successfully</h1>");
+		req.getRequestDispatcher("login.html").include(req,resp);
+	}else {
+		resp.getWriter().print("<h1 style='color:red'>Sorry!! Account Cannot be Created</h1>");
+		req.getRequestDispatcher("signup.html").include(req, resp);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*resp.getWriter().print("<h1>");
+	resp.getWriter().print("Name: "+"<br>");
+	resp.getWriter().print("Email: "+email+"<br>");
+	resp.getWriter().print("Password: "+password+"<br>");
+	resp.getWriter().print("Mobile: "+mobile+"<br>");
+	resp.getWriter().print("DOB: "+dob+"<br>");
+	resp.getWriter().print("Gender: "+gender+"<br>");
+	resp.getWriter().print("age: "+age+"<br>");*/
+} 
+}
---
+++ src/main/webapp/Home.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<style>
+div{
+		display:flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+	</style>
+</head>
+<body>
+     <div>
+     <h1>TODO Home</h1>
+     </div>
+</body>
+</html>
